593423d6cf3042c75fc82fb7386bd1fa86737576
maiyasser <maiyasser@localhost.localdomain>
2022-12-14 22:03:59 +0200

    header-CreateTable-done

--- header.dbms.sh
+++ header.dbms.sh
@@ -3,11 +3,13 @@ function createTable {
 	echo -e "Enter Tablename: \c"
 	read tableName
 
-	if [ -f "LocalDBs"/$1/$tableName ]
+	if [ -f "LocalDBs"/$1/$tableName"_meta.db"]
 	then
 		echo "Table already exist"
 		#callmenufun
-	else
+	else 
+	touch "LocalDBs"/$1/$tableName"_meta.db"
+	touch "LocalDBs"/$1/$tableName".db" 
 		echo -e "Enter number of columns: \c"
 		read ColNum
 		FieldSep=":"
@@ -18,17 +20,17 @@ function createTable {
 
 			do
 				echo -e  "Enter Column Name: \c"
-				read colName
+				read ColName
 				echo "Enter Column Type: "
 				while true
 					do
 					select choice in "int" "varchar"
 					do
 						case $REPLY in
-						1) colType="int"
+						1) ColType="int"
 							break
 							;;
-						2) colType="varchar"
+						2) ColType="varchar"
 							break
 							;;
 						*) echo "invalid choice"
@@ -36,15 +38,35 @@ function createTable {
 					esac
 					done
 		                done
-				
+ 			if [ $PK="" ] || [ $PK -eq 0 ]
+			then
+			echo  "Is it Primary Key?"
+			while true
+			do
+			echo -e " 1 : YES , 2 : No \c"			
+			read 
+				case $REPLY in
+				1) PK=1
+				   meta=$meta$RecordSep$ColName$FieldSep$ColType"1"      						break		;;
+				2) PK=0
+				   meta=$meta$RecordSep$ColName$FieldSep$ColType"0"
+					break
+							;;
+				*) echo "invalid choice"
+							;;
+				esac
+			done		
+
+			else
+			meta=$meta$RecordSep$ColName$FieldSep$ColType"0"
+			fi
 
 
 
+		done
+		
+		echo -e $meta >> "LocalDBs"/$1/$tableName"_meta.db"
 
-
-			done
-
-
-
-		fi
+	fi
+#displayMenu
 }
